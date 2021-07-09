d24f9a95220c5c50b36087c6ce3f4145219d9c27
alfonsus20 <alfonsusavianto@student.ub.ac.id>
2021-07-09 21:19:02 +0700

    fix: quick access responsive

--- src/components/quick-access/QuickAccess.tsx
+++ src/components/quick-access/QuickAccess.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { motion } from "framer-motion";
-// import { Dispatch } from "redux";
 import { QUICK_ACCESS_FEATURES } from "../../utils/constants";
 import QuickAccessCard from "./QuickAccessCard";
-// import { useDispatch, useSelector } from "react-redux";
-// import { RootState } from "../../store";
 import Underline from "../Underline";
-// import {
-//   HIDE_QUICKSTART,
-//   QuickStartDispatchTypes,
-// } from "../../store/constants/quickStartConstants";
+import { XIcon } from "@heroicons/react/outline";
 
 export type QuickAccessProps = {
   shown: boolean;
@@ -17,9 +11,6 @@ export type QuickAccessProps = {
 };
 
 const QuickAccess = ({ shown, showQuickAccess }: QuickAccessProps) => {
-  // const dispatch: Dispatch<QuickStartDispatchTypes> = useDispatch();
-  // const { shown } = useSelector((state: RootState) => state.quickStart);
-
   const variants = {
     shown: {
       maxHeight: "auto",
@@ -41,7 +32,7 @@ const QuickAccess = ({ shown, showQuickAccess }: QuickAccessProps) => {
       transition={{ stiffness: 0, duration: 0.5 }}
       variants={variants}
     >
-      <div className="relative my-auto w-full px-20 mx-auto pb-12">
+      <div className="relative my-auto w-full px-20 mx-auto md:pb-12">
         <div className="relative">
           <div className="mb-8">
             <h2 className="text-4xl font-medium text-center mt-16 mb-3">
@@ -49,27 +40,8 @@ const QuickAccess = ({ shown, showQuickAccess }: QuickAccessProps) => {
             </h2>
             <Underline backgroundColor="#fff" width={60} height={2} center />
           </div>
-          <button
-            className="absolute right-0 top-0"
-            onClick={() => showQuickAccess(false)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
         </div>
-        <div className="grid grid-cols-6 gap-2 px-4">
+        <div className="grid grid-cols-2 sm:grid-cols-6 gap-2 px-4">
           {QUICK_ACCESS_FEATURES.map((feature, i) => (
             <QuickAccessCard
               key={i}
@@ -80,6 +52,12 @@ const QuickAccess = ({ shown, showQuickAccess }: QuickAccessProps) => {
           ))}
         </div>
       </div>
+      <button
+        className="absolute right-8 top-8"
+        onClick={() => showQuickAccess(false)}
+      >
+        <XIcon className="w-8 h-8 text-white" />
+      </button>
     </motion.div>
   );
 };
--- src/components/quick-access/QuickAccessCard.tsx
+++ src/components/quick-access/QuickAccessCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { QuickAccessFeature } from "../../utils/constants";
 import { QuickAccessProps } from "./QuickAccess";
+import { ArrowRightIcon } from "@heroicons/react/outline";
 
 const QuickAccessCard = ({
   title,
@@ -21,37 +22,24 @@ const QuickAccessCard = ({
       } `}
     >
       <div
-        className={`${
+        className={`col-span-1 ${
           colspan === 1
-            ? "col-span-1"
+            ? "md:col-span-1"
             : colspan === 2
-            ? "col-span-2"
-            : "col-span-3"
-        } text-center h-60 bg-cover bg-center flex flex-col items-center justify-center transition transform duration-150 ease-out hover:scale-110 `}
+            ? "md:col-span-2"
+            : "md:col-span-3"
+        } text-center h-28 sm:h-40 md:h-60 bg-cover bg-center flex flex-col items-center justify-center transition transform duration-150 ease-out hover:scale-110 `}
         style={{
           backgroundImage: `linear-gradient(to bottom, rgba(17, 8, 40, 0.4), rgba(17, 8, 40, 0.6) ),url(assets/pictures/${image}.jpg)`,
         }}
       >
-        <h3 className="text-xl font-medium">{title}</h3>
+        <h3 className="text-md md:text-xl font-medium">{title}</h3>
         <Link
           to={path}
           className="mt-2 border-2 border-white rounded-full p-1 transition transform duration-150 ease-in-out hover:scale-110"
           onClick={() => showQuickAccess(false)}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M17 8l4 4m0 0l-4 4m4-4H3"
-            />
-          </svg>
+         <ArrowRightIcon className='h-4 w-4 md:h-6 md:w-6'/>
         </Link>
       </div>
     </div>
